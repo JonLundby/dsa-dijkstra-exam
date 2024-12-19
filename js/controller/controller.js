6b0071644679f6409df82b88a0c1a5788c26c226
Jon <lundbyjon@hotmail.com>
2024-12-19 02:21:30 +0100

    added grid resize model & visual via control panel

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,10 +1,39 @@
 "use strict";
-import { initView } from "../view/view.js";
+import Grid from "../model/grid.js";
+import * as view from "../view/view.js";
+
+export { GRID_ROWS_SIZE, GRID_COLS_SIZE };
 
 window.addEventListener("load", startApp);
 
+// globale variabler
+let GRID_ROWS_SIZE;
+let GRID_COLS_SIZE;
+let model;
+
 function startApp() {
-    console.log("controller say hi");
+    // sætter GRID_ROWS_SIZE & GRID_COLS_SIZE til default value
+    GRID_ROWS_SIZE = parseInt(document.querySelector("#row-size-input").value); 
+    GRID_COLS_SIZE = parseInt(document.querySelector("#col-size-input").value);
     
-    initView();
+    // Ny instans a grid model
+    model = new Grid(GRID_ROWS_SIZE, GRID_ROWS_SIZE, 0);
+
+    resizeGridModel()
+    
+    // opdatere model med GRID_ROWS_SIZE & GRID_COLS_SIZE hvis bruger input ændrer sig
+    document.querySelector("#row-size-input").addEventListener("change", resizeGridModel);
+    document.querySelector("#col-size-input").addEventListener("change", resizeGridModel);
+    
+    
+}
+
+// ændre grid størrelse og opdaterer model
+function resizeGridModel() {
+    GRID_ROWS_SIZE = parseInt(document.querySelector("#row-size-input").value); 
+    GRID_COLS_SIZE = parseInt(document.querySelector("#col-size-input").value);
+    model = new Grid(GRID_ROWS_SIZE, GRID_COLS_SIZE, 0);
+    console.table(model.grid);
+
+    view.createVisualGrid(GRID_ROWS_SIZE, GRID_COLS_SIZE);
 }
--- js/view/view.js
+++ js/view/view.js
@@ -1,5 +1,26 @@
-export { initView }
+import { GRID_ROWS_SIZE, GRID_COLS_SIZE } from "../controller/controller.js";
 
-function initView() {
-    console.log("View says hi");
+export { createVisualGrid, updateVisualCell }
+
+function createVisualGrid(rows, cols) {
+    const visualGrid = document.querySelector("#grid-container");
+    visualGrid.innerHTML = "";
+
+    // opdater css root værdier --grid-rows & --grid-cols
+    document.documentElement.style.setProperty("--grid-rows", rows)
+    document.documentElement.style.setProperty("--grid-cols", cols)
+
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+
+            visualGrid.insertAdjacentElement("beforeend", cell);
+        }
+    }
+}
+
+//
+function updateVisualCell(cell, value) {
+    // kode til at ændre visualCell baseret på en value
 }
